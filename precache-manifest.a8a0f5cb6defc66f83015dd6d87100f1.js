83315d530f3a3064ac7f0a206f4e60325a9f1b8e
purequote <70182226+purequote@users.noreply.github.com>
2020-09-01 21:24:24 +0100

    Updates

---
+++ precache-manifest.a8a0f5cb6defc66f83015dd6d87100f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5ffc83988a7eddd2fe2fd74ed2aa91",
+    "url": "/dnd-strife/index.html"
+  },
+  {
+    "revision": "50676e8274cdfa1a54d9",
+    "url": "/dnd-strife/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80aa45a02e94ace2bdab",
+    "url": "/dnd-strife/static/js/2.963ac434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-strife/static/js/2.963ac434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50676e8274cdfa1a54d9",
+    "url": "/dnd-strife/static/js/main.2eff4a83.chunk.js"
+  },
+  {
+    "revision": "99dd48928b5cece5b868",
+    "url": "/dnd-strife/static/js/runtime-main.0fc750e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-strife/static/media/logo.5d5d9eef.svg"
+  }
+]);
